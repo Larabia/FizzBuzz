def9d72b6a1ebf30b8e8c8a24dfc5f505fa996ab
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-08-05 17:50:03 -0300

    solution 2 for loop

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
 $count = 1;
+<?php
+
+$count = 1;
+
+//solution with while/if
 
 while ($count<101){
 
@@ -20,3 +25,31 @@ while ($count<101){
   $count++;
 
 }
+
+
+//solution with for/foreach
+
+$output = [];
+
+for ($i = 1; $i <= 100; $i++){
+
+  if($i % 15 === 0){
+    array_push($output, "FizzBuzz");
+
+  } elseif ($i % 5 === 0){
+    array_push($output, "Buzz");
+
+  } elseif ($i % 3 === 0){
+    array_push($output, "Fizz");
+  
+  } else {
+    array_push($output, $i);
+  }
+
+}
+
+foreach ($output as $value) {
+  echo $value . "\n";
+}
+
+
